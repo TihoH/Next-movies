198d50a575337502444d061088ba717a7b4cc4d9
unknown <https://github.com/TihoH>
2025-05-31 14:45:52 +0300

    edit mair11ff13fqqf1r1rn

--- app/pageFilm/[id]/page.tsx
+++ app/pageFilm/[id]/page.tsx
@@ -7,34 +7,30 @@ import { getActors } from "@/API/getActors";
 import Image from "next/image";
 import BTNFavorites from "@/components/UI/BTNFavorites";
 
-// type PageProps = {
-//   params: {
-//     id: string;
-//   };
-// };
-
-const PageFilm = async ({ params }: { params: { id: string } }) => {
+export default async function PageFilm({
+  params,
+}: {
+  params: { id: string };
+}) {
   const id = Number(params.id);
   const data = await getFilmById(id, "movie");
   const trailer = await getTrailer(id, "movie");
   const actors = await getActors("movie", id);
 
-  console.log(typeof id);
-
   return (
     <Container>
-      <div className="relative z-10 py-10 flex ">
+      <div className="relative z-10 py-10 flex">
         <div>
           <Image
-            className="rounded-lg border border-green-800 "
+            className="rounded-lg border border-green-800"
             src={`https://image.tmdb.org/t/p/w780/${data?.poster_path}`}
             alt=""
             width={400}
             height={300}
           />
           <BTNFavorites
-            text={"Добавить в избранное"}
-            className="mt-3 "
+            text="Добавить в избранное"
+            className="mt-3"
             isHover={false}
           />
         </div>
@@ -47,12 +43,10 @@ const PageFilm = async ({ params }: { params: { id: string } }) => {
         </div>
       </div>
       <img
-        className="w-full  object-cover object-center absolute top-0 left-0 right-0  opacity-25 border-b border-green-500"
+        className="w-full object-cover object-center absolute top-0 left-0 right-0 opacity-25 border-b border-green-500"
         src={`https://image.tmdb.org/t/p/w780/${data?.backdrop_path}`}
         alt=""
       />
     </Container>
   );
-};
-
-export default PageFilm;
+}
